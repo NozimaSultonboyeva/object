a7c608a00383586beb469d8ff3dd5c66da85a067
NozimaSultonboyeva <sultonboyevanozima34@gmail.com>
2023-11-04 14:26:26 +0300

    first project

---
+++ app.js
@@ -0,0 +1,38 @@
+let str =`Nozima Sultonboyeva`
+let str2 ="Robiya"
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.slice(0,7));
+console.log(str.replace("Nozima", "Robiya"));
+console.log(str.concat(str2));
+console.log(str.indexOf("i"));
+console.log(str.trim());
+console.log(str.charAt(0));
+console.log(str.split(""));
+console.log(str.includes("Nozima"));
+
+
+
+
+
+
+// let i =+prompt("Son kiriting men kvadratini topib beraman")
+
+//  if(i){
+// console.log(i**2 );
+// console.log('kvadrati' );
+
+// }else{
+//     console.log("Son kiriting");
+// }
+// const son =+prompt("Son kiriting")
+// function kvadrat_hisobla(son){
+//     if(son){
+//         let kvadrat=son**2;
+//         alert(son+"sonning kvadrati" +kvadrat)
+//     } else {
+//         alert("Iltimos raqam kiriting!")
+//     }
+// }
+// kvadrat_hisobla(son);
